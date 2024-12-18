0a12974220f7dbb0e9896a5afb710bfb5166b93b
David Calabrese <David.thomas.Calabrese@gmail.com>
2024-12-17 23:30:46 -0600

    Fix nav listener bug, add active link styling to side nav panel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,62 @@
-(function($) {
+(function ($) {
 
-	var	$window = $(window),
-		$body = $('body');
+    var $window = $(window),
+        $body = $('body');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '1000px'  ],
-			small:   [ null,      '736px'  ]
-		});
+    // Breakpoints.
+    breakpoints({
+        xlarge: ['1281px', '1680px'],
+        large: ['981px', '1280px'],
+        medium: ['737px', '1000px'],
+        small: [null, '736px']
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Play initial animations on page load.
+    $window.on('load', function () {
+        window.setTimeout(function () {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			offsetY: -22,
-			mode: 'fade',
-			noOpenerFade: true,
-			speed: 300,
-			detach: false
-		});
+    // Dropdowns.
+    $('#nav > ul').dropotron({
+        offsetY: -22,
+        mode: 'fade',
+        noOpenerFade: true,
+        speed: 300,
+        detach: false
+    });
 
-	// Nav.
+    // Nav.
 
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
+    // Title Bar.
+    $(
+        '<div id="titleBar">' +
+        '<a href="#navPanel" class="toggle"></a>' +
+        '<span class="title"></span>' +
+        '</div>'
+    )
+        .appendTo($body);
 
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+    // Panel.
+    $(
+        '<div id="navPanel">' +
+        '<nav>' +
+        $('#nav').navList() +
+        '</nav>' +
+        '</div>'
+    )
+        .appendTo($body)
+        .panel({
+            delay: 500,
+            hideOnClick: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'left',
+            target: $body,
+            visibleClass: 'navPanel-visible'
+        });
 
 })(jQuery);
 
@@ -65,15 +65,24 @@ const currentPath = window.location.pathname.split("/").pop();
 const navLinks = document.querySelectorAll('#nav a');
 
 navLinks.forEach(link => {
-  if (link.getAttribute('href') === currentPath) {
-    // Add 'active' class to the parent `<li>` of the link
-    link.parentElement.classList.add('active');
+    if (link.getAttribute('href') === currentPath) {
+        // Add 'active' class to the parent `<li>` of the link
+        link.parentElement.classList.add('activeNav');
 
-    // Add 'active' class to all parent `<li>` elements for nested links
-    let parent = link.parentElement.parentElement.closest('li');
-    while (parent) {
-      parent.classList.add('active');
-      parent = parent.parentElement.closest('li');
+        // Add 'active' class to all parent `<li>` elements for nested links
+        let parent = link.parentElement.parentElement.closest('li');
+        while (parent) {
+            parent.classList.add('activeNav');
+            parent = parent.parentElement.closest('li');
+        }
     }
-  }
 });
+
+// sidebar links (for smaller breakpoints)
+const navPanelLinks = document.querySelectorAll('#navPanel a');
+navPanelLinks.forEach(link => {
+    if (link.getAttribute('href') === currentPath) {
+        link.classList.add('activeNavPanel');
+    }
+});
+
